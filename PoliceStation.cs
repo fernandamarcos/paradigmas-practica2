cdb37d1447e4deb64f612393bd1ecabb1c9397cc
Fernanda <202202335@alu.comillas.edu>
2024-09-28 15:48:07 +0200

    Create Police Station

--- PoliceStation.cs
+++ PoliceStation.cs
@@ -1,4 +1,29 @@
-﻿namespace Practice1
-{
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Practice1
+{
+    class PoliceStation
+    {
+        public int stationNumber;   
+        private List<PoliceCar> PoliceCarList {  get; set; }
+        public bool alarm; 
+
+        public PoliceStation(int stationNumber) 
+        { 
+            this.stationNumber= stationNumber;
+            alarm = false;
+            PoliceCarList = new List<PoliceCar>();
+        }
+
+        public void RegisterNewCar(PoliceCar car)
+        {
+            PoliceCarList.Add(car);
+            
+        }
+
+    }
 }
